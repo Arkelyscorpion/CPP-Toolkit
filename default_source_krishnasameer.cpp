73c1cfe9af6b72c849fc7d020981f2d8fe167c21
Krishna Sameer <krishnasameer51@gmail.com>
2022-12-29 20:19:14 +0530

    Added Default Source only

---
+++ default_source_krishnasameer.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb emplace_back
+#define sz(v) (int)(v.size())
+#define all(v) (v.begin(),v.end())
+
+signed main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	// This is not Complete yet :P
+	return 0;
+}
